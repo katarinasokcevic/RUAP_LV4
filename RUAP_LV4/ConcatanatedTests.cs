2d2260631d431cee74114a55fd550c2c81d085ae
unknown <test>
2022-12-09 14:57:28 +0100

    Tests concatanated and fixed asserts

--- RUAP_LV4/ConcatanatedTests.cs
+++ RUAP_LV4/ConcatanatedTests.cs
@@ -10,13 +10,13 @@ using OpenQA.Selenium.Support.UI;
 namespace RUAP_LV4
 {
     [TestFixture]
-    public class ProvjeraOdlaskaNaOdabirNaInaPlaAnjaKrozKoAricu
+    public class ConcatanatedTests
     {
         private IWebDriver driver;
         private StringBuilder verificationErrors;
         private string baseURL;
         private bool acceptNextAlert = true;
-        
+
         [SetUp]
         public void SetupTest()
         {
@@ -24,7 +24,7 @@ namespace RUAP_LV4
             baseURL = "https://www.google.com/";
             verificationErrors = new StringBuilder();
         }
-        
+
         [TearDown]
         public void TeardownTest()
         {
@@ -38,7 +38,40 @@ namespace RUAP_LV4
             }
             Assert.AreEqual("", verificationErrors.ToString());
         }
-        
+
+        [Test]
+        public void ThePrijavaRegKorisnikaTest()
+        {
+            driver.Navigate().GoToUrl("https://demowebshop.tricentis.com/");
+            driver.FindElement(By.LinkText("Log in")).Click();
+            driver.FindElement(By.Id("Email")).Clear();
+            driver.FindElement(By.Id("Email")).SendKeys("vexoni1298@covbase.com");
+            driver.FindElement(By.Id("Password")).Click();
+            driver.FindElement(By.Id("Password")).Clear();
+            driver.FindElement(By.Id("Password")).SendKeys("sifrasifra123");
+            driver.FindElement(By.Id("Password")).SendKeys(Keys.Enter);
+        }
+
+        [Test]
+        public void TheProvjeraDodavanjaArtikalaUKoAricuTest()
+        {
+            driver.Navigate().GoToUrl("https://demowebshop.tricentis.com/");
+
+            driver.FindElement(By.LinkText("Log in")).Click();
+            driver.FindElement(By.Id("Email")).Clear();
+            driver.FindElement(By.Id("Email")).SendKeys("vexoni1298@covbase.com");
+            driver.FindElement(By.Id("Password")).Click();
+            driver.FindElement(By.Id("Password")).Clear();
+            driver.FindElement(By.Id("Password")).SendKeys("sifrasifra123");
+            driver.FindElement(By.XPath("//input[@value='Log in']")).Click();
+            driver.FindElement(By.XPath("//div[3]/div/div[2]/div[3]/div[2]/input")).Click();
+            
+            //+++
+            Thread.Sleep(5000);
+
+            driver.FindElement(By.XPath("//li[@id='topcartlink']/a/span")).Click();
+        }
+
         [Test]
         public void TheProvjeraOdlaskaNaOdabirNaInaPlaAnjaKrozKoAricuTest()
         {
@@ -52,13 +85,14 @@ namespace RUAP_LV4
             driver.FindElement(By.XPath("//input[@value='Log in']")).Click();
             driver.FindElement(By.XPath("//li[@id='topcartlink']/a/span[2]")).Click();
             driver.FindElement(By.Id("CountryId")).Click();
-            var test = driver.Url;
             new SelectElement(driver.FindElement(By.Id("CountryId"))).SelectByText("Croatia");
             driver.FindElement(By.XPath("//option[@value='24']")).Click();
             driver.FindElement(By.Id("StateProvinceId")).Click();
             driver.FindElement(By.Id("checkout")).Click();
             driver.FindElement(By.XPath("//button/span")).Click();
-            
+
+            //+++
+            Assert.AreEqual(driver.Url, "https://demowebshop.tricentis.com/onepagecheckout");
         }
         private bool IsElementPresent(By by)
         {
@@ -72,7 +106,7 @@ namespace RUAP_LV4
                 return false;
             }
         }
-        
+
         private bool IsAlertPresent()
         {
             try
@@ -85,18 +119,25 @@ namespace RUAP_LV4
                 return false;
             }
         }
-        
-        private string CloseAlertAndGetItsText() {
-            try {
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
                 IAlert alert = driver.SwitchTo().Alert();
                 string alertText = alert.Text;
-                if (acceptNextAlert) {
+                if (acceptNextAlert)
+                {
                     alert.Accept();
-                } else {
+                }
+                else
+                {
                     alert.Dismiss();
                 }
                 return alertText;
-            } finally {
+            }
+            finally
+            {
                 acceptNextAlert = true;
             }
         }
